85361d422ec77b78f09cee0598807fe192df7c84
Smith Ringtho <sringtho@gmail.com>
2022-03-24 20:21:27 +0300

    Added the search input to usestate

--- frontend/src/components/Search.js
+++ frontend/src/components/Search.js
@@ -1,15 +1,20 @@
 import React from "react";
 import { Col, Row, Form, Button, Container } from "react-bootstrap";
 
-const Search = () => {
+const Search = ({ word, setWord, handleSubmit }) => {
   return (
     <Container className="mt-4">
       <Row className="justify-content-center">
         <Col xs={12} md={8} lg={6}>
-          <Form>
+          <Form onSubmit={handleSubmit}>
             <Row>
               <Col xs={9}>
-                <Form.Control placeholder="Search for Image..." />
+                <Form.Control
+                  type="text"
+                  value={word}
+                  onChange={(e) => setWord(e.target.value)} 
+                  placeholder="Search for Image..." 
+                />
               </Col>
               <Col>
                 <Button variant="dark" type="submit">Search</Button>
